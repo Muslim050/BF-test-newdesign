bf82d3cd04b9bd48ac17672d7e8ff81e9dd47e4a
Muslim.Yakhyayev <Muslim.Yakhyayev@ucell.uz>
2024-07-14 09:16:51 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,58 +1,16 @@
-<<<<<<< HEAD
 // import path from 'path';
-// import react from '@vitejs/plugin-react';
-// import { defineConfig } from 'vite';
-//
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, './src'),
-//       'src': path.resolve(__dirname, './src'), // Убрали слэш в конце
-//     },
-//   },
-//   server: {
-//     port: 3000,
-//   },
-// });
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-
-import { fileURLToPath } from 'url';
-import path from 'path'; // Add this line to import the path module
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-
-// Convert import.meta.url to a file path
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-=======
-import path from 'path';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-
->>>>>>> cf464ec (first commit)
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-<<<<<<< HEAD
-      'src': path.resolve(__dirname, './src'),
-=======
-      'src': path.resolve(__dirname, './src'), // Убрали слэш в конце
->>>>>>> cf464ec (first commit)
+      src: path.resolve(__dirname, './src'), // Убрали слэш в конце
     },
   },
   server: {
     port: 3000,
   },
-<<<<<<< HEAD
-  build: {
-    outDir: 'dist',
-  },
-});
-
-=======
-});
->>>>>>> cf464ec (first commit)
+})
