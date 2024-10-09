cf330ba5ba804af4543829065e752e23d98607e2
Muslim.Yakhyayev <Muslim.Yakhyayev@ucell.uz>
2024-10-09 10:20:08 +0500

    refactoring sidebar v4

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 import React from 'react'
 import { ThemeContext } from '@/utils/ThemeContext.jsx'
 import { gsap } from 'gsap'
-import SystemLayout from './SystemLayout';
+import SystemLayout from "@/pages/Home/SystemLayout.jsx";
 
 function Home() {
   const { pathname } = useLocation()
@@ -20,12 +20,7 @@ function Home() {
         duration: 1.5,
       })
     }
-
-    // Анимация изменения фона для боковой панели (если нужно)
-  }, [bgColor, textColor])
-  // const layout = localStorage.getItem('react-resizable-panels:layout:mail')
-  // const collapsed = localStorage.getItem('react-resizable-panels:collapsed')
-
+    }, [bgColor, textColor])
   // const defaultLayout = 300 // Инициализация значением по умолчанию
 
   return (
@@ -36,40 +31,7 @@ function Home() {
         </Routes>
       ) : (
         <div>
-          <SystemLayout   />
-          {/* <div
-            className="p-[14px] h-screen overflow-hidden"
-            style={{
-              display: 'flex',
-              backgroundColor: bgColor,
-              color: textColor,
-            }}
-            ref={contentRef}
-          >
-            <Sidebar />
-
-            <div
-              style={{
-                width: '100%',
-                // height: '100vh',
-                // overflow: 'auto',
-              }}
-              className="ml-7"
-            >
-              <Navbar />
-
-              <div className="wrapper_home">
-                <div
-                  style={{
-                    width: '100%',
-                    margin: ' 0 auto',
-                  }}
-                >
-                  <Outlet />
-                </div>
-              </div>
-            </div>
-          </div> */}
+          <SystemLayout/>
         </div>
       )}
     </>
